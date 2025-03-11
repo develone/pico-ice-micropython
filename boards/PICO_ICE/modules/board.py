0d6761b6dccb4d7c7065c3476fc0b9c08d5901b6
Vdragon <mail@massdriver.space>
2025-03-11 16:12:46 +0100

    Update to latest module

--- boards/PICO_ICE/modules/board.py
+++ boards/PICO_ICE/modules/board.py
@@ -1,16 +1,13 @@
 import machine, os
 
-fpga_spi = None
 pmod_spi = None
 i2c = None
 fpga_uart = None
 
 if "pico2-ice" in os.uname().machine:
-	fpga_spi = machine.SPI(0)
 	pmod_spi = machine.SPI(1)
 	i2c = machine.I2C(1)
 elif "pico-ice" in os.uname().machine:
-	fpga_spi = machine.SPI(1)
 	pmod_spi = machine.SPI(0)
 	i2c = machine.I2C(0)
 	fpga_uart = machine.UART(0)
